3516236a7577499a6d3991637dea93cf4768bd80
SitongHui <492150523@qq.com>
2018-08-06 08:57:58 +0800

    修改

--- js/index.js
+++ js/index.js
@@ -3,18 +3,20 @@ window.onload = function () {
 };
 
 function dialog() {
-    var $ = document.querySelectorAll.bind(document);
     //获取元素
+    var $ = document.querySelectorAll.bind(document);
     var confirmBtnEle = $('.dialog > .confirmbtn')[0];
     var inputTitleEle = $('.dialog > .title')[0];
     var showDialogEle = $('.dialog > .showdialog')[0];
-    var closeBtnEle = $('.dialog > .showdialog > .closebtn')[0];
+    var cancelBtnEle = $('.dialog > .showdialog > .cancelbtn')[0];
+    var closeBtnEle = $('.dialog > .showdialog > .dialogtitle > .closebtn')[0];
     var checkTitleEle = $('.dialog > .showdialog > .dialogcontent > .checktitle')[0];
     var showDialogStatus = false;
+
     //注册事件
     elementDisplay(confirmBtnEle, 'block');
-    elementDisplay(closeBtnEle, 'none');
-
+    elementDisplay(cancelBtnEle, 'none');
+    elementDisplay(closeBtnEle,'none');
 
     //工具函数
     function elementDisplay(ele, status) {
@@ -25,6 +27,7 @@ function dialog() {
             appendELe(checkTitleEle);
         },false)
     }
+
     function appendELe(ele) {
         var p = document.createElement('p');
         p.innerHTML = inputTitleEle.value;
